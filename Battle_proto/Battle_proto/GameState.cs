b96c68df9503ee4cc27de35ceac3d89dfcc7ee4d
BBBB29-creator <coree09@naver.com>
2026-07-09 13:30:07 +0900

    GameState 추가
    원활한 씬 이동을 위한 스크립트 파일 생성. 현재 전투, 인벤토리, 상점 3가지만 enum으로 표기하였다.

---
+++ Battle_proto/Battle_proto/GameState.cs
@@ -0,0 +1,10 @@
+﻿// GameState.cs 파일 내용
+namespace RealtimeAtbRpg
+{
+    public enum GameState
+    {
+        Battle,     // 전투 중
+        Inventory,  // 인벤토리 창
+        Shop        // 상점 창
+    }
+}
