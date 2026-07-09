62a8a23561c626b5720a7f5f8f8aac90b14c0040
BBBB29-creator <coree09@naver.com>
2026-07-09 14:24:13 +0900

    GameState 항목 추가
    전투 승리 후, 상점으로 갈지 다음 전투를 속행할지 고르는 선택문 추가

--- Battle_proto/Battle_proto/GameState.cs
+++ Battle_proto/Battle_proto/GameState.cs
@@ -3,8 +3,9 @@ namespace RealtimeAtbRpg
 {
     public enum GameState
     {
-        Battle,     // 전투 중
-        Inventory,  // 인벤토리 창
-        Shop        // 상점 창
+        Battle,      // 전투 중
+        Inventory,   // 인벤토리 창
+        SelectNext,  // 💡 전투 승리 후 [1: 상점 / 2: 다음 전투] 선택 대기 상태
+        Shop         // 💡 상점 창
     }
 }
